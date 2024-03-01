e63cecf95f2b757d72de2b1809c9383efd6011ba
joa@rio-soft.ru <joa@rio-soft.ru>
2024-03-01 14:25:25 +0500

    6

--- UnitTestProject1/UnitTestProject1/Test1.cs
+++ UnitTestProject1/UnitTestProject1/Test1.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using OpenQA.Selenium;
+using Docker.DotNet.Models;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
-namespace ssv_web_tests
+public class UntitledTestCase
 {
-    [TestClass]
-    public class Test1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    JavascriptExecutor js;
+  }
+
+@Test
+  public void testUntitledTestCase() Throws Exception
+{
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.id("logo")).click();
+    acceptNextAlert = true;
+    driver.findElement(By.id("4")).click();
+    driver.findElement(By.xpath("//input[@value='Delete']")).click();
+    assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
+    driver.findElement(By.id("logo")).click();
+    driver.findElement(By.name("username")).click();
+    driver.findElement(By.name("password")).click();
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("postgres");
+    driver.findElement(By.id("f12d670948")).click();
     }
+
+    
 }
+
